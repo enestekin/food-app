92abdb698bf0015f7a73e08cab137ae8fbc06a04
enestekin <zenestekin@gmail.com>
2022-02-05 03:22:32 +0300

    input ui and component added

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MealItemForm from './MealItemForm';
 import styles from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -10,7 +11,9 @@ const MealItem = (props) => {
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
